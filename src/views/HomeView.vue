c9fe8d00e62a24487cc3496051593645e15556ca
Piotr Jasiński <piotr.jasinski2002@gmail.com>
2023-04-24 12:28:32 +0200

    widok aplikacji todo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,9 @@
 
 <template>
   <main>
-    <TheWelcome />
+    <h1> LISTA TODO:</h1>
+    <p>Dodaj nowe zadanie: {{ message }}</p>
+    <input v-model="message" placeholder="zadanie" />
+    
   </main>
 </template>
