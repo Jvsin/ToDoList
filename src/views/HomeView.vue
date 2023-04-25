018bc78b53436752a268085e96a45f460bf6101d
Piotr Jasiński <piotr.jasinski2002@gmail.com>
2023-04-25 11:02:11 +0200

    dodawanie zadań

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,34 @@
-<script setup>
+<script>
 
+let id = 0
+export default {
+  data() {
+    return {
+      lista: [],
+      nowyElement: ''
+    }
+  },
+  methods: {
+    dodajElement() {
+      this.lista.push({id: id++, task: this.nowyElement});
+      this.nowyElement = '';
+    },
+  }
+}
 </script>
 
 <template>
   <main>
     <h1> LISTA TODO:</h1>
     <p>Dodaj nowe zadanie: {{ message }}</p>
-    <input v-model="message" placeholder="zadanie" />
     
+
+    <input type="text" v-model="nowyElement">
+      <button @click="dodajElement">Dodaj</button>
+      <ul>
+        <li v-for = "element in lista" :key = "element.id">{{ element.task }}
+    
+        </li>
+      </ul>
   </main>
 </template>
